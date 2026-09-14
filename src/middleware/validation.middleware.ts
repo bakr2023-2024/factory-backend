5fc17fce24e978d1d6224904811082d9d6e01215
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-09-14 20:28:59 +0300

    setup validation middleware

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodObject } from "zod";
+import { BadRequestException } from "../utils/exceptions";
+type ReqKey = keyof Request;
+type Schema = Partial<Record<ReqKey, ZodObject>>;
+const validation = (schema: Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const errors: {
+      key: ReqKey;
+      issues: { message: string; path: PropertyKey | undefined }[];
+    }[] = [];
+    for (const key of Object.keys(schema) as ReqKey[]) {
+      if (!schema[key] || !req[key]) continue;
+      const result = schema[key].safeParse(req[key]);
+      if (!result.success) {
+        errors.push({
+          key,
+          issues: result.error.issues.map((issue) => ({
+            message: issue.message,
+            path: issue.path[0],
+          })),
+        });
+      }
+      if (errors.length)
+        throw new BadRequestException("Validation Error", { errors });
+      next();
+    }
+  };
+};
+export default validation;
