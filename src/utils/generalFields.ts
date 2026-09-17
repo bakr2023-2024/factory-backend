f99a858f9961fe8fd2235da05264a3ca0ab6f12f
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-09-17 13:51:24 +0300

    create reusable validation objects

---
+++ src/utils/generalFields.ts
@@ -0,0 +1,29 @@
+import z from "zod";
+export const fields = {
+  id: z
+    .string()
+    .refine((val) => val.trim() && !isNaN(Number(val)) && Number(val) > 0, {
+      message: "ID can only be a positive integer",
+    })
+    .transform((val) => Number(val)),
+  name: z.string().min(3).max(100),
+
+  paginate: z.strictObject({
+    page: z
+      .string()
+      .refine((val) => val.trim() && !isNaN(Number(val)) && Number(val) > 0, {
+        message: "page can only be a positive integer",
+      })
+      .transform((val) => Number(val))
+      .default(1),
+    size: z
+      .string()
+      .refine((val) => val.trim() && !isNaN(Number(val)) && Number(val) > 0, {
+        message: "skip can only be a positive integer",
+      })
+      .transform((val) => Number(val))
+      .default(0),
+    search: z.string().min(2).optional(),
+  }),
+};
+export type paginateDTO = z.infer<typeof fields.paginate>;
