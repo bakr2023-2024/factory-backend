ab7a2cbda75a4dc17c09eb3c7c54afaea27f8c32
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-09-18 21:22:12 +0300

    override Prisma.Decimal toJson function to return a number instead of string

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
+import { Prisma } from "./generated/prisma/client";
+(Prisma.Decimal.prototype as any).toJSON = function () {
+  return this.toNumber();
+};
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
